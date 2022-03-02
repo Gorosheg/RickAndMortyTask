b4e3453a06ca163c79473fdd849762bab883f74e
AnnaG <anutagoroxowa@yandex.ru>
2022-03-02 16:42:39 +0300

    Add: Fragments

---
+++ feature/episodes/src/main/java/gorosheg/episodes/presentation/Episodes.kt
@@ -0,0 +1,12 @@
+package gorosheg.episodes.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import gorosheg.episodes.R
+
+class Episodes : Fragment(R.layout.fragment_episodes) {
+
+}
